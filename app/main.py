f0f48b5f4b4f613e118b975c8b2b86dfd3f1c89d
code-project <dev@code-check.io>
2018-06-01 07:18:00 +0000

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+def main(argv):
+    # このコードは引数と標準出力を用いたサンプルコードです。
+    # このコードは好きなように編集・削除してもらって構いません。
+    # ---
+    # This is a sample code to use arguments and outputs.
+    # Edit and remove this code as you like.
+
+    for i, v in enumerate(argv):
+        if v == "World" :
+            print ("Hello World!")
+        elif v == "織田信長":
+                print ("Hello 織田信長!")
+        elif v == "codecheck":
+            print ("Hello codecheck!")
+                    
